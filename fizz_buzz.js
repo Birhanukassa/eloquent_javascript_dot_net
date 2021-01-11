c5edd347d80f922bc5496d9318019ba8a50390e8
Birhanu <ricardogebremichael@gmail.com>
2021-01-11 02:19:08 -0600

    Update fizz_buzz.js

--- fizz_buzz.js
+++ fizz_buzz.js
@@ -18,4 +18,22 @@ for (let n = 1; n <= 100; n++) {
   console.log(output || n);
 }
 
+//
+
+let FizzBuzz = countDown => {
+  for (num = 1; num <= 100; num++) {
+    if (num % 3 === 0 && num % 5 === 0 ) {
+        console.log("FizzBuzz"); 
+    } else if (num % 3 === 0) {
+         console.log("Buzz");
+    }else {
+      if ((!num % 3) === 0 && num % 5 === 0) {
+        console.log("Fizz");     
+      }  
+    } 
+  } 
+
+}
+
+FizzBuzz();
 
